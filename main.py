cccd87fb0e9c0c027447781ede28a28a93d3b1dd
twoaxxx-cmyk <twoaxxx@gmail.com>
2026-03-13 21:51:44 +0300

    add menu

--- main.py
+++ main.py
@@ -8,13 +8,19 @@ bot = telebot.TeleBot(TOKEN)
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     bot.reply_to(message, "Привет! Я твой первый бот и я работаю!")
-@bot.message_handler(commands=['help
-def send_help(message):
-    bot.reply_to(message, "Я умею отвечать на /start и повторять за тобой.")
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
     bot.reply_to(message, f"Ты написал: {message.text}")
+
+@bot.message_handler(commands=['help'])
+def send_help(message):
+    help_text = "Я умею:\n"
+    help_text += "/start - приветствие\n"
+    help_text += "/help - список команд\n"
+    help_text += "Напиши 'Привет' или 'Пока'\n"
+    help_text += "Или отправь пример: 2+3"
+    bot.reply_to(message, help_text)
 
 print("✅ Бот запущен! Иди в Telegram и напиши /start")
 bot.infinity_polling()
