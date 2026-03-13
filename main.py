525cf6ea720c52166ecb65dafc14288df883a5e2
twoaxxx-cmyk <twoaxxx@gmail.com>
2026-03-13 21:30:59 +0300

    add hellp

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ bot = telebot.TeleBot(TOKEN)
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     bot.reply_to(message, "Привет! Я твой первый бот и я работаю!")
+@bot.message_handler(commands=['help'])
+def send_help(message):
+    bot.reply_to(message, "Я умею отвечать на /start и повторять за тобой.")
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
